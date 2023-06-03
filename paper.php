80f1d362109415a1aa02dd2340a996ab47c4794c
tiger4th <tiger4th@gmail.com>
2023-06-04 00:10:01 +0900

    元号修正

--- paper.php
+++ paper.php
@@ -3,7 +3,7 @@
 <div id="header">
 <p class="x-small">
 <span class="bold"><?php if($page=='top'){echo '1';}else{echo $key+2;} ?></span>　　
-平成<?php echo (date("Y")-1988); ?>年（<?php echo date("Y年");?>）<?php echo date("n月j日"); ?>　<?php echo $day[date("w")]; ?>曜日
+令和<?php echo (date("Y")-2018); ?>年（<?php echo date("Y年");?>）<?php echo date("n月j日"); ?>　<?php echo $day[date("w")]; ?>曜日
 　　　　　　　　　は　　　て　　　ぶ　　　新　　　聞
 </p>
 </div>
@@ -14,7 +14,7 @@
 <h1><img src="./image/logo.gif" alt="はてぶ新聞"></h1>
 <br /><br />
 <?php if(isset($date)){ ?><a href="<?php echo $wiki; ?>" target="_blank"><?php } ?>
-<p>平成<?php echo (date("Y")-1988); ?>年（<?php echo date("Y年");?>）</p>
+<p>令和<?php echo (date("Y")-2018); ?>年（<?php echo date("Y年");?>）</p>
 <p class="bold"><span class="xx-large"><?php echo date("n"); ?></span><span class="large">月</span> <span class="xx-large"><?php echo date("j"); ?></span><span class="large">日</span></p>
 <p class="large bold"><?php echo $day[date("w")]; ?>曜日</p>
 <p><?php if(isset($date)){ ?><?php echo $date; ?><?php } ?></p>
