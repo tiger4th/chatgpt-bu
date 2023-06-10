07fb7b14923f1efff251e7adbffc405df3225e93
tiger4th <tiger4th@gmail.com>
2023-06-10 18:52:25 +0900

    インデント修正

--- script.php
+++ script.php
@@ -1,36 +1,36 @@
 <script type="text/javascript" src="./js/h2v-min.js" charset="utf-8"></script>
 <script type="text/javascript">
-var params = {
-  'rotate_99':{
-    'fontsize': '14px',
-    'chars': 25,
-    'lineInterval': 0.3,
-    'pagewidth': '800px',
-    'fontSet': 'mincho',
-    'auto': true,
-    'showcredit': false,
-    'bar': false,
-    'multiCols': true,
-    'splash': false
-  },
-}
-
-h2vconvert.init(params);
-
-function rotate() {
-  if (document.getElementById("vertical").value == 1) {
-    document.getElementById("vertical").value = 0;
-  } else {
-    document.getElementById("vertical").value = 1;
+  var params = {
+    'rotate_99':{
+      'fontsize': '14px',
+      'chars': 25,
+      'lineInterval': 0.3,
+      'pagewidth': '800px',
+      'fontSet': 'mincho',
+      'auto': true,
+      'showcredit': false,
+      'bar': false,
+      'multiCols': true,
+      'splash': false
+    },
   }
 
-  hash = location.hash;
-  hash = hash.replace("#", "");
-  if (document.getElementById("vertical").value != document.getElementById("state_" + hash).value) {
-    h2vconvert.switcher('rotate_' + document.getElementById("code_" + hash).value);
-    document.getElementById("state_" + hash).value = document.getElementById("vertical").value;
+  h2vconvert.init(params);
+
+  function rotate() {
+    if (document.getElementById("vertical").value == 1) {
+      document.getElementById("vertical").value = 0;
+    } else {
+      document.getElementById("vertical").value = 1;
+    }
+
+    hash = location.hash;
+    hash = hash.replace("#", "");
+    if (document.getElementById("vertical").value != document.getElementById("state_" + hash).value) {
+      h2vconvert.switcher('rotate_' + document.getElementById("code_" + hash).value);
+      document.getElementById("state_" + hash).value = document.getElementById("vertical").value;
+    }
   }
-}
 </script>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-8QXZ5NTVSD"></script>
