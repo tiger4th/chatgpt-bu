fb0d186e1f10e0f51390e4936d3d1a496ebfffee
tiger4th <tiger4th@gmail.com>
2023-06-11 19:27:06 +0900

    印刷用xml対応

--- logic.php
+++ logic.php
@@ -6,8 +6,13 @@ $name = ['記事'];
 $long_name = ['記事'];
 $day = ['日', '月', '火', '水', '木', '金', '土'];
 
+$source = 'https://note.com/chatgpt_nobdata/rss?fbclid=IwAR2oayu5iQXct8TrRGz8xh4_Jk2JgVr8OpjI5_Z72xkO_ma6xyICyDi1oMk';
+if (isset($_GET['source'])) {
+  $source = './xml/' . $_GET['source'] . '.xml';
+}
+
 foreach ($list as $category) {
-  $xml = file_get_contents('https://note.com/chatgpt_nobdata/rss?fbclid=IwAR2oayu5iQXct8TrRGz8xh4_Jk2JgVr8OpjI5_Z72xkO_ma6xyICyDi1oMk');
+  $xml = file_get_contents($source);
   $feed = simplexml_load_string($xml);
   for ($i = 0; $i < 12; $i++) {
     $title = $feed->channel->item[$i]->title;
